08ab597805b5bc03a642a5e6227222738fcd7c30
BrianSS99 <brianss99@hotmail.com>
2021-04-20 01:06:07 -0500

    Mapa correcion 1.0

--- resources/views/Reportes/mapa.blade.php
+++ resources/views/Reportes/mapa.blade.php
@@ -151,10 +151,16 @@
         console.log(estrat4_array);
         
         let cont1=0;
+        let aux1="";
         for(let i=0; i<estrat1_array.length; i++){
             c.beginPath();
             c.font="16px Times New Roman";
-            c.fillText(estrat1_array[i],310+cont1,650,1000);
+            if(estrat1_array[i].length>20){
+                c.fillText(estrat1_array[i].substring(0,20),310+cont1,650,1000);
+                c.fillText(estrat1_array[i].substring(20,estrat1_array[i].length),310+cont1,660,1000);
+            }else{
+                c.fillText(estrat1_array[i],310+cont1,650,1000);
+            }
             c.strokeStyle="blue";
             c.arc(380+cont1,650,69,0, Math.PI * 2, false);
             c.stroke();
@@ -165,7 +171,13 @@
         for(let i=0; i<estrat2_array.length; i++){
             c.beginPath();
             c.font="16px Times New Roman";
-            c.fillText(estrat2_array[i],330+cont2,510,1000);
+            if(estrat2_array[i].length>20){
+                c.fillText(estrat2_array[i].substring(0,20),330+cont2,510,1000);
+                c.fillText(estrat2_array[i].substring(20,estrat2_array[i].length),330+cont2,520,1000);
+            }else{
+                c.fillText(estrat2_array[i],330+cont2,510,1000);
+            }
+            // c.fillText(estrat2_array[i],330+cont2,510,1000);
             c.strokeStyle="orange";
             c.arc(400+cont2,510,69,0, Math.PI * 2, false);
             c.stroke();
@@ -177,7 +189,13 @@
         for(let i=0; i<estrat3_array.length; i++){
             c.beginPath();
             c.font="16px Times New Roman";
-            c.fillText(estrat3_array[i],350+cont3,370,1000);
+            if(estrat3_array[i].length>20){
+                c.fillText(estrat3_array[i].substring(0,20),350+cont3,370,1000);
+                c.fillText(estrat3_array[i].substring(20,estrat3_array[i].length),350+cont3,380,1000);
+            }else{
+                c.fillText(estrat3_array[i],350+cont3,370,1000);
+            }
+            // c.fillText(estrat3_array[i],350+cont3,370,1000);
             c.strokeStyle="yellow";
             c.arc(420+cont3,370,69,0, Math.PI * 2, false);
             c.stroke();
@@ -188,7 +206,13 @@
         for(let i=0; i<estrat4_array.length; i++){
             c.beginPath();
             c.font="16px Times New Roman";
-            c.fillText(estrat4_array[i],370+cont4,230,1000);
+            if(estrat4_array[i].length>20){
+                c.fillText(estrat4_array[i].substring(0,20),370+cont4,230,1000);
+                c.fillText(estrat4_array[i].substring(20,estrat4_array[i].length),370+cont4,240,1000);
+            }else{
+                c.fillText(estrat4_array[i],370+cont4,230,1000);
+            }
+            // c.fillText(estrat4_array[i],370+cont4,230,1000);
             c.strokeStyle="green";
             c.arc(440+cont4,230,69,0, Math.PI * 2, false);
             c.stroke();
@@ -196,100 +220,7 @@
         }
 
 
-        // c.beginPath();
-        // c.font="18px Verdana";
-        // c.fillText('Objetivo',253,275,1000);
-        // c.font="16px Times New Roman";
-        // c.fillText(objetivo,243,310,1000);
-        // c.arc(70+x,300,60,0, Math.PI * 2, false);
-        // c.strokeStyle = 'black';
-        // c.stroke();
-
-        // c.beginPath();
-        // c.arrow(130+x,300, 200+x, 300, [0, 1, -10, 1, -10, 5]);
-        // c.fill();
-
-        // c.font="18px Verdana";
-        // c.fillText('Indicador',460,275,1000);
-        // c.font="14px Times New Roman";
-        // if(nombre2 != ""){
-        //     c.fillText(nombre.substring(0,29),420,310,1000);
-        //     c.fillText(nombre2,420,323,1000 )
-        // }else{
-        //     c.fillText(nombre,420,310,1000);
-        // }
-        // c.strokeRect(200+x,250,170,100);
-
-        // c.beginPath();
-        // c.arrow(370+x,300, 440+x, 300, [0, 1, -10, 1, -10, 5]);
-        // c.fill();
-
-        // c.beginPath();
-        // c.font="18px Verdana";
-        // c.fillText('Meta',695,275,1000);
-        // c.font="14px Times New Roman";
-        // c.fillText(meta,670,310,1000);
-        // c.arc(500+x,300,60,0, Math.PI * 2, false);
-        // c.stroke();
-
-        // c.beginPath();
-        // c.arrow(560+x,300, 600+x, 300, [0, 1, -10, 1, -10, 5]);
-        // c.fill();
-
-        // c.font="18px Verdana";
-        // c.fillText('Semaforo',860,265,1000);
-        // c.strokeRect(600+x,240,170,115);
-        // c.beginPath();
-        // c.arc(840,285,10,0, Math.PI * 2, false);
-        // c.fillStyle = 'red';
-        // c.fill();
-        // c.font="14px Times New Roman";
-        // c.fillText(rojo,855,288,1000);
-        // c.beginPath();
-        // c.arc(840,310,10,0, Math.PI * 2, false);
-        // c.fillStyle = 'yellow';
-        // c.fill();
-        // c.font="14px Times New Roman";
-        // c.fillText(amarillo,855,313,1000);
-        // c.beginPath();
-        // c.arc(840,340,10,0, Math.PI * 2, false);
-        // c.fillStyle = 'green';
-        // c.fill();
-        // c.font="14px Times New Roman";
-        // c.fillText(verde,855,343,1000);
-
-        // c.fillStyle = "black";
-        // c.beginPath();
-        // c.arrow(240+x,400, 240+x, 350, [0, 1, -10, 1, -10, 5]);
-        // c.fill();
-
-        // c.beginPath();
-        // c.arrow(490+x,400, 490+x, 360, [0, 1, -10, 1, -10, 5]);
-        // c.fill();
-
-
-        // c.font="18px Verdana";
-        // c.fillText('Iniciativas',540,420,1000);
-        // c.font="14px Times New Roman";
-        // let cont = 0;
-        // for(let i = 0; i<iniciativas.length; i++){
-        //     c.fillText(iniciativas[i].value,170+x,440+cont,1000);
-        //     cont= cont +20;
-        // }
-        // c.strokeRect(170+x,400,410,115);
-
-        // c.beginPath();
-        // c.arrow(610+x,460, 580+x, 460, [0, 1, -10, 1, -10, 5]);
-        // c.fill();
-
-        // c.font="18px Verdana";
-        // c.fillText('Responsable',860,430,1000);
-        // c.font="14px Times New Roman";
-        // c.fillText(responsable,610+x,460,1000);
-        // c.strokeRect(610+x,410,170,100);
-
-
-        // console.log(canvas);
+        
 
     </script>
 @endsection
